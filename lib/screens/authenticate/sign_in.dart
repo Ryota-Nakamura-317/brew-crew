9245a866eef96bbe793c7e73273c45c5612e4329
中村遼太 <nkmr94317rt@gmail.com>
2021-01-13 10:00:11 +0900

    #13 - Sign in With Email & Password

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -13,10 +13,12 @@ class _SignInState extends State<SignIn> {
   //データ呼び出し専用のauth.dartの呼び出し
   //これでAuthServiceがこのクラスでも使用可能となる
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   //text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -41,34 +43,57 @@ class _SignInState extends State<SignIn> {
       body: Container(
         //EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0)；縦と横に空白を設定できる。
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Column(
-          children: [
-            SizedBox(height: 20.0),
-            TextFormField(
-              onChanged: (val) {
-                setState(() => email = val);
-              },
-            ),
-            SizedBox(height: 20.0),
-            TextFormField(
-              obscureText: true,
-              onChanged: (val) {
-                setState(() => password = val);
-              },
-            ),
-            SizedBox(height: 20.0),
-            RaisedButton(
-              color: Colors.pink[400],
-              child: Text(
-                'Sign in',
-                style: TextStyle(color: Colors.white),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              SizedBox(height: 20.0),
+              TextFormField(
+                //①入力フォーム内の表示文字とその判断
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
               ),
-              onPressed: () async {
-                print(email);
-                print(password);
-              },
-            ),
-          ],
+              SizedBox(height: 20.0),
+              TextFormField(
+                obscureText: true,
+                //②入力フォーム内の表示文字とその判断,六文字以上のパスワードの作成
+                validator: (val) =>
+                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              RaisedButton(
+                color: Colors.pink[400],
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  //上の①②を判断し、どちらともnullであればtrueとなり、処理が実行される
+                  if (_formKey.currentState.validate()) {
+                    dynamic result =
+                        await _auth.signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() =>
+                          error = 'could not sign in with those credentials');
+                    }
+                  }
+                },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
