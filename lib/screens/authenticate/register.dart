f9adae3f283bf995c6273cf6290af8b5981a7bd6
中村遼太 <nkmr94317rt@gmail.com>
2021-01-13 11:34:27 +0900

    #15 - Loading Widget

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -16,6 +17,7 @@ class _RegisterState extends State<Register> {
   //これでAuthServiceがこのクラスでも使用可能となる
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   //text field state
   String email = '';
@@ -24,86 +26,95 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        //elevation； 高さ、影をつける
-        elevation: 0.0,
-        title: Text('Sign up to Brew Crew'),
-        //appbar内のSignInボタン
-        actions: [
-          FlatButton.icon(
-            onPressed: () {
-              //toggleViewの値を参照して判断
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-          ),
-        ],
-      ),
-      body: Container(
-        //EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0)；縦と横に空白を設定できる。
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              SizedBox(height: 20.0),
-              TextFormField(
-                //decorationプロパティ
-                //textInputDecorationを別で定義して呼び出す形にすれば、他でコピペが簡単にできる
-                //.copyWith(hintText: 'Email')をつけることでtextInputDecorationに対してhintTextをそれぞれに定義できる
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                //①入力フォーム内の表示文字とその判断
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                //constants.dartより持ってくる
-                decoration: textInputDecoration.copyWith(hintText: 'password'),
-                obscureText: true,
-                //②入力フォーム内の表示文字とその判断,六文字以上のパスワードの作成
-                validator: (val) =>
-                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  'Register',
-                  style: TextStyle(color: Colors.white),
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              //elevation； 高さ、影をつける
+              elevation: 0.0,
+              title: Text('Sign up to Brew Crew'),
+              //appbar内のSignInボタン
+              actions: [
+                FlatButton.icon(
+                  onPressed: () {
+                    //toggleViewの値を参照して判断
+                    widget.toggleView();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('Sign In'),
                 ),
-                onPressed: () async {
-                  //上の①②を判断し、どちらともnullであればtrueとなり、処理が実行される
-                  if (_formKey.currentState.validate()) {
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                        email, password);
-                    if (result == null) {
-                      setState(() => error = 'Please supply a valid email');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
+              ],
+            ),
+            body: Container(
+              //EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0)；縦と横に空白を設定できる。
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      //decorationプロパティ
+                      //textInputDecorationを別で定義して呼び出す形にすれば、他でコピペが簡単にできる
+                      //.copyWith(hintText: 'Email')をつけることでtextInputDecorationに対してhintTextをそれぞれに定義できる
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      //①入力フォーム内の表示文字とその判断
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      //constants.dartより持ってくる
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'password'),
+                      obscureText: true,
+                      //②入力フォーム内の表示文字とその判断,六文字以上のパスワードの作成
+                      validator: (val) => val.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        //上の①②を判断し、どちらともnullであればtrueとなり、処理が実行される
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .registerWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Please supply a valid email';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -15,6 +16,8 @@ class _SignInState extends State<SignIn> {
   //これでAuthServiceがこのクラスでも使用可能となる
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  //ローディングの判断、falseはローディング必要なし
+  bool loading = false;
 
   //text field state
   String email = '';
@@ -23,84 +26,96 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        //elevation； 高さ、影をつける
-        elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
-        //appbar内のRegisterボタン
-        actions: [
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-          ),
-        ],
-      ),
-      body: Container(
-        //EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0)；縦と横に空白を設定できる。
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              SizedBox(height: 20.0),
-              TextFormField(
-                //constants.dartより持ってくる
-                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                //①入力フォーム内の表示文字とその判断
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                //constants.dartより持ってくる
-                decoration: textInputDecoration.copyWith(hintText: 'password'),
-                obscureText: true,
-                //②入力フォーム内の表示文字とその判断,六文字以上のパスワードの作成
-                validator: (val) =>
-                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
+    //loadingがtrueの時はLoadingを表示させ、falseの場合は画面表示をさせる
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
+              backgroundColor: Colors.brown[400],
+              //elevation； 高さ、影をつける
+              elevation: 0.0,
+              title: Text('Sign in to Brew Crew'),
+              //appbar内のRegisterボタン
+              actions: [
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('Register'),
                 ),
-                onPressed: () async {
-                  //上の①②を判断し、どちらともnullであればtrueとなり、処理が実行される
-                  if (_formKey.currentState.validate()) {
-                    dynamic result =
-                        await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() =>
-                          error = 'could not sign in with those credentials');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0,
+              ],
+            ),
+            body: Container(
+              //EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0)；縦と横に空白を設定できる。
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      //constants.dartより持ってくる
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'Email'),
+                      //①入力フォーム内の表示文字とその判断
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      //constants.dartより持ってくる
+                      decoration:
+                          textInputDecoration.copyWith(hintText: 'password'),
+                      obscureText: true,
+                      //②入力フォーム内の表示文字とその判断,六文字以上のパスワードの作成
+                      validator: (val) => val.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 20.0),
+                    RaisedButton(
+                      color: Colors.pink[400],
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        //上の①②を判断し、どちらともnullであればtrueとなり、処理が実行される
+                        if (_formKey.currentState.validate()) {
+                          //ボタン押下後にloadingをtrueにしてローディングを出す。
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              //入力にエラーがあればloadingを再びfalseにかえす
+                              loading = false;
+                              error =
+                                  'could not sign in with those credentials';
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 12.0),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 14.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
